8e33b5a3da9ab800f309c05784219b2e20ea17b9
studiousOldMan <studiousOldMan@outlook.com>
2023-10-10 00:48:41 -0700

    fixed the column issue

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import datetime
 
 # region - Get top
 data_top_stories = requests.get(
@@ -12,7 +13,12 @@ data_top_stories_2 = data_top_stories.json()
 # endregion
 
 # region - Loop Through Stores
+## Create a blank dataframe
 z_temp_data = pd.DataFrame()
+
+## Create a perfect list
+z_perfect_list = ['by', 'descendants', 'id', 'score', 'time', 'title', 'type', 'url', 'kids']
+
 for i in range(0, len(data_top_stories_2)):
     print(
         data_top_stories_2[i]
@@ -26,11 +32,15 @@ for i in range(0, len(data_top_stories_2)):
     # Extract content and convert to json
     z_data_2 = z_data.json()
 
-    # TODO 1
-        #Convert z_data_2 to dataframe 
+    # Extract keys of z_data_2
+    z_data_2_keys = list(z_data_2.keys())
 
-    # TODO 2
-        # extract columns and check if kids presents or whatever column is missing.
+    # Check if any missing
+    z_data_2_keys_2_missing = list(set(z_perfect_list) - set(z_data_2_keys))
+
+    # If missing, create a new key and blank value
+    for j in range(0, len(z_data_2_keys_2_missing)):
+        z_data_2[z_data_2_keys_2_missing[j]] = ''
 
     # Remove kids as we want 1 row result
     z_data_2_kids = z_data_2.pop('kids')
@@ -50,3 +60,6 @@ for i in range(0, len(data_top_stories_2)):
     )
 
 # endregion
+
+# region - add a datetime column
+z_temp_data['extracted_datetime'] = datetime.datetime.now()
